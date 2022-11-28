2235862f7a82b1bb8bac07c038b6bcbc40aa7d75
aisyah-17 <c030320004@mahasiswa.poliban.ac.id>
2022-11-28 09:35:15 +0800

    layout dikit

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -80,8 +80,8 @@
         <aside class="main-sidebar sidebar-light-red elevation-4">
             <a href="/" class="brand-link" style="background-color: #dc3545">
                 <img src="{{ asset('AdminLTE/dist/img/ta bulat.png') }}" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-bold" style="color: white">Telkom Akses Sehat</span>
+                    class="brand-image img-circle elevation-3" style="opacity: ">
+                <span class="brand-text font-weight:500" style="color: white">Telkom Akses Sehat</span>
             </a>
 
             <!-- Sidebar -->
@@ -195,7 +195,7 @@
     <!-- AdminLTE App -->
     <script src="{{ asset('AdminLTE/dist/js/adminlte.js') }}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('AdminLTE/dist/js/pages/dashboard2.js') }}"></script>
+    {{-- <script src="{{ asset('AdminLTE/dist/js/pages/dashboard2.js') }}"></script> --}}
     <!-- DataTables  & Plugins -->
     <script src="{{ asset('AdminLTE/plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
@@ -209,6 +209,7 @@
     <script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
     <script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    @yield('script')
     <script>
         $(function() {
             $('#table2').DataTable({
